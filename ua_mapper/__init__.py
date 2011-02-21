bf60a52b854b5d7dfb719079cda09a2db98b339c
Max Naude <max@unomena.com>
2011-02-22 01:02:34 +0200

    MD5 the user agent when getting a device type from Redis

--- ua_mapper/__init__.py
+++ ua_mapper/__init__.py
@@ -1,6 +1,9 @@
+import hashlib
+
 import redis
 
 from django.conf import settings
+
 
 #------------------------------------------------------------------------------
 def get_server(host_port=None):
@@ -19,10 +22,12 @@ def get_server(host_port=None):
 
 #------------------------------------------------------------------------------
 def get_template_set(user_agent_string, host_port=None):
-    return get_server(host_port).get(user_agent_string)
+    prefix = getattr(settings, 'UA_MAPPER_KEY_PREFIX', '')
+    user_agent_md5 = hashlib.md5(user_agent_string).hexdigest()
+    key = "%s%s" % (prefix, user_agent_md5)
+    return get_server(host_port).get(key)
 
 
 if __name__ == '__main__':
-    
     print get_template_set('Mozilla/5.0 (Linux; U; Android 1.6; pl-pl; Era G1 Build/DRC92) AppleWebKit/525.10+ (KHTML, like Gecko) Version/3.0.4 Mobile Safari/523.12.2',
                            '127.0.0.1:6379')
